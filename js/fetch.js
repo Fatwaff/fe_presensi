c9a73c085db09294ecafeee0c7d751d275db5686
Fatwa <fax.mp4@gmail.com>
2023-03-31 20:38:38 +0700

    up

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,6 @@
-import { get } from "https://bukulapak.github.io/api/process.js"; 
-let urlAPI = "https://ws-fatwa.herokuapp.com/presensi";
-get(urlAPI,isiTablePresensi);
-function isiTablePresensi(results){
-    console.log(results);
+import { get } from "https://bukulapak.github.io/api/process.js";
+let urlAPI = "https://ws-fatwa.herokuapp.com/presensi2";
+get(urlAPI, isiTablePresensi);
+function isiTablePresensi(results) {
+  console.log(results);
 }
